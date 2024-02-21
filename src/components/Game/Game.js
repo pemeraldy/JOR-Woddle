b8fb1e5a7474867ec630c9ae206279aa55142058
Peter Olu <pemeraldy@gmail.com>
2024-02-21 08:04:04 +0100

    👌 IMPROVE: improve logic to check and set game status

--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import GuessInput from "../GuessInput/GuessInput";
 import { sample } from "../../utils";
 import { WORDS } from "../../data";
+import { NUM_OF_GUESSES_ALLOWED } from "../../constants";
 import GuessResult from "../GuessResult/GuessResult";
 
 // Pick a random word on every pageload.
@@ -11,18 +12,28 @@ console.info({ answer });
 
 function Game() {
   const [guesses, setGuesses] = useState([]);
+  const [gameStatus, setGameStatus] = useState('running');
 
   const handleNewGuess = (potentialGuess) => {
-    setGuesses([
-      ...guesses,
-      potentialGuess
-    ]);
+    const nextGuesses = [...guesses, potentialGuess]
+    setGuesses(
+      nextGuesses
+    );
+    if(potentialGuess.toUpperCase() === answer){
+      setGameStatus('won')
+      return
+    }
+    if (nextGuesses.length >= NUM_OF_GUESSES_ALLOWED) {
+      setGameStatus("lost");
+      return;
+    }
     // console.log(potentialGuess, guesses);
   };
   return (
     <>
+      {gameStatus}
       <GuessResult guessList={guesses} answer={answer} />
-      <GuessInput handleNewSubmit={handleNewGuess} />
+      <GuessInput disabled={gameStatus !== 'running' } handleNewSubmit={handleNewGuess} />
     </>
   );
 }
