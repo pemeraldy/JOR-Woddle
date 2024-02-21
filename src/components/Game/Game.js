1acceab214d7da613e76b1bd191ca1b3670ff115
Peter Olu <pemeraldy@gmail.com>
2024-02-21 08:40:43 +0100

    👌 IMPROVE: game over and won

--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -5,6 +5,9 @@ import { WORDS } from "../../data";
 import { NUM_OF_GUESSES_ALLOWED } from "../../constants";
 import GuessResult from "../GuessResult/GuessResult";
 
+import LostBanner from "../LostBanner/LostBanner";
+import WonBanner from "../WonBanner/WonBanner";
+
 // Pick a random word on every pageload.
 const answer = sample(WORDS);
 // To make debugging easier, we'll log the solution in the console.
@@ -12,28 +15,30 @@ console.info({ answer });
 
 function Game() {
   const [guesses, setGuesses] = useState([]);
-  const [gameStatus, setGameStatus] = useState('running');
+  const [gameStatus, setGameStatus] = useState("running");
 
   const handleNewGuess = (potentialGuess) => {
-    const nextGuesses = [...guesses, potentialGuess]
-    setGuesses(
-      nextGuesses
-    );
-    if(potentialGuess.toUpperCase() === answer){
-      setGameStatus('won')
-      return
+    const nextGuesses = [...guesses, potentialGuess];
+
+    setGuesses(nextGuesses);
+
+    if (potentialGuess.toUpperCase() === answer) {
+      setGameStatus("won");
+      return;
     }
+
     if (nextGuesses.length >= NUM_OF_GUESSES_ALLOWED) {
       setGameStatus("lost");
       return;
     }
-    // console.log(potentialGuess, guesses);
   };
   return (
     <>
       {gameStatus}
       <GuessResult guessList={guesses} answer={answer} />
-      <GuessInput disabled={gameStatus !== 'running' } handleNewSubmit={handleNewGuess} />
+      <GuessInput disabled={gameStatus !== "running"} handleNewSubmit={handleNewGuess} />
+      {gameStatus === "lost" && <LostBanner answer={answer} />}
+      {gameStatus === "won" && <WonBanner numOfGuesses={guesses.length} />}
     </>
   );
 }
